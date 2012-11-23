be3b102cf765c7ee622b4dad2e3f114acbb10c62
paasar <paasar@gmail.com>
2012-11-23 15:47:56 +0200

    Add added. And main refactored to be more extendable.

--- src/betterjson.hs
+++ src/betterjson.hs
@@ -1,13 +1,22 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {- TODO:
+    modify value
+    delete value
+    add tag
+    remove tag
     pretty print
       output options:
         sort
         show tags
     special tags (date)
+    check argument amount
+    date to items
   -}
+import qualified Control.Exception as C
 import System.Environment(getArgs)
+import System.Directory
 import System.IO
+import System.IO.Error
 import Text.JSON
 import Text.JSON.Generic
 import Text.Regex(splitRegex, mkRegex)
@@ -25,45 +34,75 @@ data Item = Item {
 type Tag = String
 
 ---------------------------------------
+syntaxMsg = "Syntax: <path to file> [add value/find] tag1[:tag2...]"
 
+
+dispatch :: [(String, [String] -> IO ())]  
+dispatch =  [ ("add", addValue)  
+            , ("find", findByTags)  
+            , ("del", removeByValue)  
+            ]
+
+
+main = execute `C.catch` handler
+
+execute :: IO ()
+execute = do
+  (filename:command:args) <- getArgs
+  let (Just action) = lookup command dispatch
+  action (filename : args)
+
+handler :: IOError -> IO ()
+handler e
+  | isDoesNotExistError e = putStrLn "The file doesn't exist!"
+  | otherwise = ioError e
+
+
+
+----------------------------------
 stringToTags :: String -> [Tag]
 stringToTags tagStr = splitRegex (mkRegex ":") tagStr
 
-
-findByTags :: String -> [Tag] -> IO ()
-findByTags filepath tags = do
+-- find RAAH
+findByTags :: [String] -> IO ()
+findByTags [filepath, tagStr] = do
+  let tags = (stringToTags tagStr)
   inFile <- openFile filepath ReadMode
   contents <- hGetContents inFile
   let book = (decodeJSON contents :: Book)
   putStrLn $ show $ map value $ itemsByTags tags book
   
   hClose inFile
+findByTags _ = error "Syntax: <path to file> find tag1[:tag2...]"
   
 itemsByTags :: [Tag] -> Book -> [Item]
 itemsByTags [] _ = []
 itemsByTags (t:ts) b =
   filter (\item -> t `elem` (tags item)) (items b) ++ (itemsByTags ts b)
 
-addValue :: String -> String -> [Tag] -> IO ()
-addValue filepath value tags = do
-  putStrLn "Not yet implemented"
-
-main = do
-  {-
-  Args:
-    get: tag1:tag2:tag3
-    add: add "value" tag1:tag2
-  -}
-  args <- getArgs
-  case args of
-    [filepath, tagStr] -> findByTags filepath (stringToTags tagStr)
-    [filepath, "add", value, tagStr] -> addValue filepath value (stringToTags tagStr)
-    _ -> putStrLn "Syntax: <path to file> [add value] tag1[:tag2...]"
+-- add "val1" tag1:tag2
+addValue :: [String] -> IO ()
+addValue [filepath, value, tagStr] = do
+  let tags = stringToTags tagStr
   
-  {-
-  putStrLn "---------------------"
+  handle <- openFile filepath ReadMode
+  (tempName, tempHandle) <- openTempFile "." "temp"
+  contents <- hGetContents handle
+  
   let book = (decodeJSON contents :: Book)
-  putStrLn $ show $ book
-  putStrLn $ last $ tags $ head $ items book
-  putStrLn $ show $ map value $ itemsByTags [("ruokalista" :: Tag)] book
-  -}
+  let updatedBook = addValueToBook value tags book
+  hPutStr tempHandle $ (encodeJSON updatedBook)
+  hClose handle
+  hClose tempHandle
+  removeFile filepath
+  renameFile tempName filepath
+  putStrLn $ "Value (" ++ value ++ ") added."
+addValue _ = error "Syntax: <path to file> add value tag1[:tag2...]"
+
+addValueToBook :: String -> [Tag] -> Book -> Book
+addValueToBook value tags oldBook = oldBook {items = (createItem value tags) : (items oldBook)}
+  where createItem value tags = Item {value = value, tags = tags}
+
+removeByValue :: [String] -> IO ()
+removeByValue [filepath, value] = do
+  putStrLn "Not yet implemented."
